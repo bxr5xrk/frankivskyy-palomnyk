afa5e67c2ae431a22d31349dc78e2837a58e3741
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-04-11 13:54:55 +0300

    added some info for tour page

--- src/pages/tour/ui/TourPage.tsx
+++ src/pages/tour/ui/TourPage.tsx
@@ -1,14 +1,71 @@
 import { useTour } from '@/features/tours';
+import Spinner from '@/shared/ui/Spinner';
 import { useParams } from 'react-router-dom';
 
 export default function TourPage() {
   const { id } = useParams();
 
-  const { data: tour } = useTour(Number(id) ?? 0, { skip: !id });
+  const {
+    data: tour,
+    isLoading,
+    isFetching
+  } = useTour(Number(id) ?? 0, { skip: !id });
 
   return (
     <div>
-      <h1>{tour?.title}</h1>
+      {isLoading && isFetching ? <Spinner /> : null}
+
+      {tour ? (
+        <>
+          <div className="border-b border-gray-200 pb-10 text-center">
+            <h2 className="font-medium text-gray-500">{tour.title}</h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              {tour.extended_title}
+            </p>
+            <p className="mt-4 text-gray-500">{tour.attractions}</p>
+          </div>
+
+          {/* stats */}
+          <div className="">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+                {[
+                  { name: 'Ціна', value: tour.price },
+                  { name: 'Дата', value: tour.start_date }
+                ].map((stat) => (
+                  <div
+                    key={stat.name}
+                    className="mx-auto flex max-w-xs flex-col gap-y-4"
+                  >
+                    <dt className="text-base leading-7 text-gray-600">
+                      {stat.name}
+                    </dt>
+                    <dd className="order-first text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+                      {stat.value}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+
+          {/* images */}
+          <div className="mt-16 grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:gap-x-8">
+            {tour.images.map((image, index) => (
+              <div
+                key={index + 1}
+                className="aspect-h-2 aspect-w-3 w-full overflow-hidden rounded-lg"
+              >
+                <img
+                  src={image}
+                  alt={String(index + 1) + 'image'}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }
